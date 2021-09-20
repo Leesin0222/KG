9e340110054aef5e6ff79a351e16327a75f19113
Leesin0222 <dragon02229@naver.com>
2021-09-20 20:28:35 +0900

    📑 -- 이스터에그 찾을 시 이스터에그 Activity로 넘어가게 설정

--- kotlingrammer/app/src/main/java/com/yongjincompany/kotlingrammer/MainActivity.kt
+++ kotlingrammer/app/src/main/java/com/yongjincompany/kotlingrammer/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //화면 가득 채워 날 물먹게 하소서 아멘(?)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
         val animDrawble = binding.rootLayout.background as AnimationDrawable
@@ -26,19 +27,21 @@ class MainActivity : AppCompatActivity() {
         animDrawble.setExitFadeDuration(5000)
         animDrawble.start()
 
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, GameActivity::class.java))
+            finish()
+        }
         binding.developerBtn.setOnClickListener {
             val openURL = Intent(Intent.ACTION_VIEW)
             openURL.data = Uri.parse("https://github.com/Leesin0222")
             startActivity(openURL)
         }
-       binding.startBtn.setOnClickListener {
-            startActivity(Intent(this, GameActivity::class.java))
-            finish()
-        }
         binding.dictionaryBtn.setOnClickListener {
             startActivity(Intent(this, DictionaryActivity::class.java))
         }
-
+        binding.easteregg.setOnClickListener {
+            startActivity(Intent(this, EasterEggActivity::class.java))
+        }
 
     }
 
@@ -48,13 +51,13 @@ class MainActivity : AppCompatActivity() {
         kBuilder.setMessage("정말로 종료하시겠습니까?")
         kBuilder.setTitle("KotlinGrammer")
         kBuilder.setIcon(R.drawable.kotlin_icon)
+
         kBuilder.setPositiveButton("나가기"){
             Dialog,which->
             exit()
         }
         kBuilder.setNegativeButton("취소"){
             Dialog,which->
-
         }
         val createBuild = kBuilder.create()
         createBuild.show()
