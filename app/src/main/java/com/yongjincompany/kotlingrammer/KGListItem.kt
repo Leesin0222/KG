be50fe6b23fdc169788b4b9c7be112f292990dff
Leesin0222 <dragon02229@naver.com>
2022-04-03 10:56:12 +0900

    ⚡ :: 이미지 띄우기

--- app/src/main/java/com/yongjincompany/kotlingrammer/KGListItem.kt
+++ app/src/main/java/com/yongjincompany/kotlingrammer/KGListItem.kt
@@ -1,9 +1,7 @@
 package com.yongjincompany.kotlingrammer
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,7 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.yongjincompany.kotlingrammer.data.KG
 
@@ -20,13 +21,16 @@ import com.yongjincompany.kotlingrammer.data.KG
 @Composable
 fun KGListItem(kg: KG) {
     Card(
-        modifier = Modifier.padding(horizontal = 0.dp, vertical = 4.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(horizontal = 0.dp, vertical = 4.dp)
+            .fillMaxWidth(),
         elevation = 2.dp,
         backgroundColor = Color.White,
         shape = RoundedCornerShape(corner = CornerSize(16.dp))
     )
     {
         Row {
+            KGImage(kg = kg)
             Column(
                 modifier = Modifier
                     .padding(16.dp)
@@ -38,4 +42,16 @@ fun KGListItem(kg: KG) {
             }
         }
     }
+}
+
+@Composable
+private fun KGImage(kg: KG) {
+    Image(painter = painterResource(id = kg.KotlinImageId),
+          contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .padding(8.dp)
+            .size(76.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+    )
 }
