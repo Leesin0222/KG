d1aabb498820b409c7ff5450ab93752cba8def5a
Leesin0222 <dragon02229@naver.com>
2022-04-03 10:48:48 +0900

    ⚡ :: List 구현 완

--- app/src/main/java/com/yongjincompany/kotlingrammer/MainActivity.kt
+++ app/src/main/java/com/yongjincompany/kotlingrammer/MainActivity.kt
@@ -3,12 +3,8 @@ package com.yongjincompany.kotlingrammer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.yongjincompany.kotlingrammer.ui.theme.KotlingrammerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,13 +17,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
 @Composable
 fun MyApp() {
     Scaffold(
         content = {
-            kotlingrammerHomeContent()
+            KotlinGrammerHomeContent()
         }
     )
 }
